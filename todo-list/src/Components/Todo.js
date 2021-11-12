e41c5058f5d3909bbd63cca318175597e33ab908
Brian Moniz <bmoniz328@gmail.com>
2021-11-12 10:52:57 -0800

    edited buttons

--- todo-list/src/Components/Todo.js
+++ todo-list/src/Components/Todo.js
@@ -6,8 +6,8 @@ export default function Todo({ todo, dispatch }) {
     return (
         <div className="todo">
            <span style={ { backgroundColor: todo.complete ? "skyblue" : "pink" } } className="todoName"> {todo.name} </span>
-           <button className="btn" onClick={() => dispatch({type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id}})}>Toggle Todo</button>
-           <button className="btn" onClick={() => dispatch({type: ACTIONS.DELETE_TODO, payload: {id: todo.id}})}>Delete Todo</button>
+           <button className="btn" onClick={() => dispatch({type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id}})}>Toggle</button>
+           <button className="btn" onClick={() => dispatch({type: ACTIONS.DELETE_TODO, payload: {id: todo.id}})}>Delete</button>
         </div>
     )
 }
